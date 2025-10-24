8849228879370f2e82704cd10902d3641f1e3824
pathaksneha659--bil <pathaksneha659@gmail.com>
2025-10-24 20:43:54 +0530

    Loops

---
+++ prb02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter the number of rows"<<endl;
+    cin>>n;
+    cout<<"Enter the number of columns"<<endl;
+    cin>>m;
+    for(int i=1;i<=n;i++){
+        for(int i=1;i<=m;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+
+}
